1b603e5ef1c955536aa722eeae8b0ce0bfb81b77
kkshivank <151807291+kkshivank@users.noreply.github.com>
2026-06-20 14:20:41 +0530

    Add remotePatterns to images configuration

--- next.config.mjs
+++ next.config.mjs
@@ -7,8 +7,14 @@ const nextConfig = {
     ignoreBuildErrors: true,
   },
   images: {
-  formats: ['image/avif', 'image/webp'],
-},
+    formats: ['image/avif', 'image/webp'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'digifame.in',
+      },
+    ],
+  },
 }
 
 export default nextConfig
