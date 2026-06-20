7d11e4834e8a87ec88fd169203e1d8e984ab7324
kkshivank <151807291+kkshivank@users.noreply.github.com>
2026-06-20 14:58:39 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -7,14 +7,26 @@ const nextConfig = {
     ignoreBuildErrors: true,
   },
   images: {
-    formats: ['image/avif', 'image/webp'],
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'digifame.in',
-      },
-    ],
-  },
+  formats: ['image/avif', 'image/webp'],
+  remotePatterns: [
+    {
+      protocol: 'https',
+      hostname: 'digifame.in',
+    },
+    {
+      protocol: 'https',
+      hostname: 'media.licdn.com',
+    },
+    {
+      protocol: 'https',
+      hostname: 'custom-images.strikinglycdn.com',
+    },
+    {
+      protocol: 'https',
+      hostname: 'p-gpb8fhd4b9fbh6fy.z01.azurefd.net',
+    },
+  ],
+},
 }
 
 export default nextConfig
